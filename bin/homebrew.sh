fddc6e0034edd58190ab10be8cf7363ac86a3416
keimoriyama <keischwiiz@gmail.com>
2019-12-29 23:18:50 +0900

    backup

--- bin/homebrew.sh
+++ bin/homebrew.sh
@@ -9,36 +9,10 @@ echo "run brew update..."
 which brew >/dev/null 2>&1 && brew update
 brew upgrade 
 
-formulas=(
-    git
-    curl
-    colordiff
-    mysql
-    postgresql
-    sqlite
-    fish
-    tmux
-)
 
 echo "start brew install apps..."
-for formula in "${formulas[@]}"; do
-    brew install $formula || brew upgrade $formula
-done
 
-casks=(
-    google-chrome
-    google-japanese-ime
-    slack
-    iterm2
-)
-
-echo "start brew cask install apps..."
-for cask in "${casks[@]}"; do
-    brew cask install $cask
-done
-
-brew cleanup
-brew cask cleanup
+brew bundle 
 
 cat << END
 
