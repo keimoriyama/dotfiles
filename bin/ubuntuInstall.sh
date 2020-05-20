c562971b2a831d209ec902bd42d1c3b9cfb3942d
keimoriyama <keischwiiz@gmail.com>
2020-05-20 09:12:26 +0900

    sudoを外した

--- bin/ubuntuInstall.sh
+++ bin/ubuntuInstall.sh
@@ -2,9 +2,9 @@
 
 $ sudo apt-get update
 
-sudo apt install git
+apt install git
 
-sudo apt install build-essential libbz2-dev libdb-dev \
+apt install build-essential libbz2-dev libdb-dev \
 >   libreadline-dev libffi-dev libgdbm-dev liblzma-dev \
 >   libncursesw5-dev libsqlite3-dev libssl-dev \
 >   zlib1g-dev uuid-dev tk-dev
@@ -17,18 +17,14 @@ sudo apt install python3-pip
 pip3 install pynvim
 
 #install nerd font
-cd $HOME
-git clone --branch=master --depth 1 https://github.com/ryanoasis/nerd-fonts.git
-cd nerd-fonts
-install.sh
 
 #install zsh
-sudo apt-get install zsh
+apt-get install zsh
 chsh -s $(which zsh)
 
 # install tmux
-sudo apt-get install tmux
+apt-get install tmux
 
 #install tig
-sudo apt-get install tig
+apt-get install tig
 
