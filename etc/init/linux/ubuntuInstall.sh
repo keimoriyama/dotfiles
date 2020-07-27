66214b0a3d700e4b9c2b9f5c1a47ad3350d99a70
kei moriyama <keischwiiz@gmail.com>
2020-07-27 13:26:27 +0900

    sudoをはずした

--- etc/init/linux/ubuntuInstall.sh
+++ etc/init/linux/ubuntuInstall.sh
@@ -1,31 +1,31 @@
 #!/bin/sh
 
-sudo apt -y update
+apt -y update
 
-sudo apt -y upgrade
+apt -y upgrade
 
-sudo apt install git
+apt install git
 
-sudo apt install build-essential libbz2-dev libdb-dev \
+apt install build-essential libbz2-dev libdb-dev \
   libreadline-dev libffi-dev libgdbm-dev liblzma-dev \
   libncursesw5-dev libsqlite3-dev libssl-dev \
   zlib1g-dev uuid-dev tk-dev
 
-sudo apt install -y python3-pip
+apt install -y python3-pip
 
 # install python liblary
-sudo pip3 install pynvim
+pip3 install pynvim
 
 #install zsh
-sudo apt install -y zsh
-sudo apt install -y nodejs npm
+apt install -y zsh
+apt install -y nodejs npm
 #install vim
-sudo apt install -y vim nvim
+apt install -y vim nvim
 
 # install tmux
-sudo apt install -y tmux
+apt install -y tmux
 
 #install tig
-sudo apt install -y tig
+apt install -y tig
 
 sudo chsh -s $(which zsh)
