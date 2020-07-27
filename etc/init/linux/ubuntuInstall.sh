368c05502accfcf7f1a966be61d0bf6af3a43551
kei moriyama <keischwiiz@gmail.com>
2020-07-27 11:22:37 +0900

    sudo してみた

--- etc/init/linux/ubuntuInstall.sh
+++ etc/init/linux/ubuntuInstall.sh
@@ -1,33 +1,31 @@
 #!/bin/sh
 
-apt-get -y update
+sudo apt -y update
 
-apt-get -y  upgrade
+sudo apt -y upgrade
 
-apt install git
+sudo apt install git
 
-apt install -y build-essential libbz2-dev libdb-dev \
->   libreadline-dev libffi-dev libgdbm-dev liblzma-dev \
->   libncursesw5-dev libsqlite3-dev libssl-dev \
->   zlib1g-dev uuid-dev tk-dev
+sudo apt install build-essential libbz2-dev libdb-dev \
+  libreadline-dev libffi-dev libgdbm-dev liblzma-dev \
+  libncursesw5-dev libsqlite3-dev libssl-dev \
+  zlib1g-dev uuid-dev tk-dev
 
-apt install -y python3-pip
-
+sudo apt install -y python3-pip
 
 # install python liblary
-
-pip3 install pynvim
+sudo pip3 install pynvim
 
 #install zsh
-apt-get -y install zsh
-chsh -s $(which zsh)
-
+sudo apt install -y zsh
+sudo apt install -y nodejs npm
 #install vim
-apt-get -y install vim nvim
+sudo apt install -y vim nvim
 
 # install tmux
-apt-get -y install tmux
+sudo apt install -y tmux
 
 #install tig
-apt-get -y install tig
+sudo apt install -y tig
 
+sudo chsh -s $(which zsh)
