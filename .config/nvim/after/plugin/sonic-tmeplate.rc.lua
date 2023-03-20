35f102e80e4184b9eeaf599068bd4aa8b50db3af
keimoriyama <keischwiiz@gmail.com>
2023-03-20 17:40:03 +0900

    update settings

--- .config/nvim/after/plugin/sonic-tmeplate.rc.lua
+++ .config/nvim/after/plugin/sonic-tmeplate.rc.lua
@@ -1,5 +1,6 @@
-local status, action = pcall(require, 'vim-sonictemplate')
+-- local status, action = pcall(require, 'vim-sonictemplate')
 
-if not status then return end
+-- if not status then print("test") return end
+
 
 vim.g.sonictemplate_vim_template_dir = "~/.dotfiles/template"
