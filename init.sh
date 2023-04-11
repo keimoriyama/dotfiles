6c4035c55d4d5dd17ae380d793cd1ce1c68798d1
keimoriyama <keischwiiz@gmail.com>
2023-04-11 23:22:12 +0900

    fix lib

--- init.sh
+++ init.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-export PLATFORM
 
-sudo xattr -dr com.apple.quarantine /Applications/Sourcetree.app
-# install brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# install via brew
-brew bundle --file=./Brewfile
 # 手動でリンクを貼る
 ln -sf ~/.dotfiles/.config/ $HOME
 # 移動できたらリンクを実行する
 for f in .??*
 do
     [ "$f" = ".git" ] && continue
-
-    ln -snfv "$DOTPATH/$f" "$HOME"/"$f"
+    [ "$f" = ".config" ] && continue
+    ln -sf "~/.dotfiles/$f" "$HOME/$f"
 done
