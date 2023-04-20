2408a6d965526f4e28799cac50797c7cbc55b290
keimoriyama <keischwiiz@gmail.com>
2023-04-20 15:47:00 +0900

    add toggle terminal

---
+++ .config/nvim/after/plugin/toggleterm.rc.lua
@@ -0,0 +1,33 @@
+local status, toggleterm = pcall(require, "toggleterm")
+if not status then
+	return
+end
+
+toggleterm.setup({
+	open_mapping = [[<C-t>]],
+	direction = "vertical",
+	size = vim.o.columns * 0.4,
+})
+
+local Terminal = require("toggleterm.terminal").Terminal
+local lazygit = Terminal:new({
+	cmd = "lazygit",
+	direction = "float",
+	hidden = true,
+})
+
+function _lazygit_toggle()
+	lazygit:toggle()
+end
+
+local option = { noremap = true, silent = true }
+
+vim.api.nvim_set_keymap("n", "lg", "<cmd>lua _lazygit_toggle()<CR>", option)
+vim.api.nvim_set_keymap("t", "<C-h>", "<cmd>wincmd h<CR>", option)
+vim.api.nvim_set_keymap("t", "<C-l>", "<cmd>wincmd l<CR>", option)
+vim.api.nvim_set_keymap("t", "<C-j>", "<cmd>wincmd j<CR>", option)
+vim.api.nvim_set_keymap("t", "<C-k>", "<cmd>wincmd k<CR>", option)
+vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", option)
+vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", option)
+vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", option)
+vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", option)
