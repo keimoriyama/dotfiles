e21c67121098a5103560404b930688d8d47bdd93
keimoriyama <keischwiiz@gmail.com>
2023-05-08 13:47:22 +0900

    add

--- .config/nvim/after/plugin/git.rc.lua
+++ .config/nvim/after/plugin/git.rc.lua
@@ -1,11 +1,19 @@
 local status, git = pcall(require, "git")
-if (not status) then return end
+if not status then
+	return
+end
+
+vim.g.mapleader = " "
 
 git.setup({
-  keymaps = {
-    -- Open blame window
-    blame = "<Leader>gb",
-    -- Open file/folder in git repository
-    browse = "<Leader>go",
-  }
+	keymaps = {
+		-- Open blame window
+		blame = "<Leader>gb",
+		-- Close blame window
+		quit_blame = "q",
+		-- Open blame commit
+		blame_commit = "<CR>",
+		-- Open file/folder in git repository
+		browse = "<Leader>go",
+	},
 })
