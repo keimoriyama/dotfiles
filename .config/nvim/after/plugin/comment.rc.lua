cbc615286f0d2f585b26c80dc6e1058892ac72fa
keimoriyama <keischwiiz@gmail.com>
2023-05-08 13:49:29 +0900

    fix plugins

--- .config/nvim/after/plugin/comment.rc.lua
+++ .config/nvim/after/plugin/comment.rc.lua
@@ -1,18 +1,19 @@
-local status, comment = pcall(require, 'Comment')
+local status, comment = pcall(require, "Comment")
 if not status then
-  return
+	return
 end
 
-comment.setup(
-  {
-    ---LHS of toggle mappings in NORMAL mode
-    ---@type table
-    toggler = {
-      ---Line-comment toggle keymap
-      line = 'col',
-      ---Block-comment toggle keymap
-      block = 'cob',
-    },
-  }
-)
-
+comment.setup({
+	---LHS of toggle mappings in NORMAL mode
+	---@type table
+	toggler = {
+		---Line-comment toggle keymap
+		line = "col",
+		---Block-comment toggle keymap
+		block = "cob",
+	},
+	opleader = {
+		line = "cc",
+		block = "bc",
+	},
+})
