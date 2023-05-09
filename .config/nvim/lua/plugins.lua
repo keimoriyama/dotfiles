2ba0f8b4faf6cd9db8efc8dbe94c20d95bdb02da
keimoriyama <keischwiiz@gmail.com>
2023-05-09 10:42:16 +0900

    add GPT plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -124,18 +124,18 @@ lazy.setup({
 		dependencies = "copilot.lua",
 	},
 	-- ChatGPT
-	-- {
-	-- "jackMort/ChatGPT.nvim",
-	-- event = "VeryLazy",
-	-- config = function()
-	-- require("chatgpt").setup()
-	-- end,
-	-- dependencies = {
-	-- "MunifTanjim/nui.nvim",
-	-- "nvim-lua/plenary.nvim",
-	-- "nvim-telescope/telescope.nvim",
-	-- },
-	-- },
+	{
+		"jackMort/ChatGPT.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("chatgpt").setup()
+		end,
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+	},
 	-- terminal setting
 	{ "akinsho/toggleterm.nvim", version = "*", config = true },
 })
