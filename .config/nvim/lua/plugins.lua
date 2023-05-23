7d9899d5ca4b146c06323443389ea8111866405d
keimoriyama <keischwiiz@gmail.com>
2023-05-23 18:10:36 +0900

    commit

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -20,119 +20,115 @@ if vim.g.vscode then
 	return nil
 end
 lazy.setup({
+	-- color scheme
+	"folke/tokyonight.nvim",
+	-- status line
+	"nvim-lualine/lualine.nvim",
+	-- indent
+	"lukas-reineke/indent-blankline.nvim",
+	--	-- auto pair and tag close
+	"windwp/nvim-autopairs",
+	"windwp/nvim-ts-autotag",
+	--	-- surround
+	"kylechui/nvim-surround",
+	--	-- git
+	"dinhhuy258/git.nvim",
+	"lewis6991/gitsigns.nvim",
+	"airblade/vim-gitgutter",
+	"akinsho/git-conflict.nvim",
+	--	-- auto comment out
+	"numToStr/Comment.nvim",
+	"luochen1990/rainbow",
+	--	-- dot repeat
+	"tpope/vim-repeat",
+	--	-- linter, formatter
+	"jose-elias-alvarez/null-ls.nvim",
+	--	-- utility
 	"nvim-lua/plenary.nvim",
+	"nvim-lua/popup.nvim",
+	-- fuzzy finder
+	"nvim-telescope/telescope.nvim",
+	{
+		"nvim-telescope/telescope-file-browser.nvim",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+			"nvim-lua/plenary.nvim",
+		},
+	},
+	--	-- File icons
+	"kyazdani42/nvim-web-devicons",
+	-- treesitter
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+	{ "yioneko/nvim-yati", dependencies = "nvim-treesitter/nvim-treesitter" },
+	--	-- bufferline
+	"akinsho/nvim-bufferline.lua",
+	--	-- show color
+	"norcalli/nvim-colorizer.lua",
+	--	-- csv
+	"Decodetalkers/csv-tools.lua",
+	--	-- comment
+	"folke/todo-comments.nvim",
+	--	-- views
+	"petertriho/nvim-scrollbar",
+	--	-- keep lastest cursor position
+	"ethanholz/nvim-lastplace",
+	-- symbols outline
+	"simrat39/symbols-outline.nvim",
+	--	-- mkdir
+	"jghauser/mkdir.nvim",
+	--	---
+	"tversteeg/registers.nvim",
+	--	-- - code action list
+	"aznhe21/actions-preview.nvim",
+	--	-- - lsp
+	"neovim/nvim-lspconfig",
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	--	--- cmp
+	"onsails/lspkind-nvim", -- vscode-like pictograms
+	"L3MON4D3/LuaSnip",
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-path",
+	"hrsh7th/nvim-cmp",
+	"hrsh7th/cmp-nvim-lsp-signature-help",
+	"yutkat/cmp-mocword",
+	"hrsh7th/cmp-cmdline",
+	"ray-x/cmp-treesitter",
+	--	-- - noice
+	"folke/noice.nvim",
+	"MunifTanjim/nui.nvim",
+	"folke/trouble.nvim",
+	"rcarriga/nvim-notify",
+	-- template
+	"mattn/vim-sonictemplate",
+	-- markdown
+	{
+		"iamcco/markdown-preview.nvim",
+		build = "cd app && npm install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	},
+	"mattn/vim-maketable",
+	--- obsidian
+	"epwalsh/obsidian.nvim",
+	"BurntSushi/ripgrep",
+	--	-- mark visualization
+	"chentoast/marks.nvim",
+	--	-- copilot
+	{ "zbirenbaum/copilot.lua", build = ":Copilot auth" },
+	{
+		"zbirenbaum/copilot-cmp",
+		dependencies = "copilot.lua",
+	},
+	--	-- terminal setting
+	"uga-rosa/ugaterm.nvim",
+	--	-- sidebar
+	"sidebar-nvim/sidebar.nvim",
+	--	-- neogit
 	"TimUntersberger/neogit",
 })
---lazy.setup({
---	-- color scheme
---	"folke/tokyonight.nvim",
---	-- status line
---	"nvim-lualine/lualine.nvim",
---	-- indent
---	"lukas-reineke/indent-blankline.nvim",
---	-- auto pair and tag close
---	"windwp/nvim-autopairs",
---	"windwp/nvim-ts-autotag",
---	-- surround
---	"kylechui/nvim-surround",
---	-- git
---	"dinhhuy258/git.nvim",
---	"lewis6991/gitsigns.nvim",
---	"airblade/vim-gitgutter",
---	"akinsho/git-conflict.nvim",
---	-- auto comment out
---	"numToStr/Comment.nvim",
---	"luochen1990/rainbow",
---	-- dot repeat
---	"tpope/vim-repeat",
---	-- linter, formatter
---	"jose-elias-alvarez/null-ls.nvim",
---	-- utility
---	"nvim-lua/plenary.nvim",
---	"nvim-lua/popup.nvim",
---	-- fuzzy finder
---	"nvim-telescope/telescope.nvim",
---	{
---		"nvim-telescope/telescope-file-browser.nvim",
---		dependencies = {
---			"nvim-telescope/telescope.nvim",
---			"nvim-lua/plenary.nvim",
---		},
---	},
---	-- File icons
---	-- "kyazdani42/nvim-web-devicons",
---	-- treesitter
---	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
---	{ "yioneko/nvim-yati", dependencies = "nvim-treesitter/nvim-treesitter" },
---	-- bufferline
---	"akinsho/nvim-bufferline.lua",
---	-- show color
---	"norcalli/nvim-colorizer.lua",
---	-- csv
---	"Decodetalkers/csv-tools.lua",
---	-- comment
---	"folke/todo-comments.nvim",
---	-- views
---	"petertriho/nvim-scrollbar",
---	-- keep lastest cursor position
---	"ethanholz/nvim-lastplace",
---	-- symbols outline
---	"simrat39/symbols-outline.nvim",
---	-- mkdir
---	"jghauser/mkdir.nvim",
---	---
---	"tversteeg/registers.nvim",
---	-- - code action list
---	"aznhe21/actions-preview.nvim",
---	-- - lsp
---	"neovim/nvim-lspconfig",
---	"williamboman/mason.nvim",
---	"williamboman/mason-lspconfig.nvim",
---	--- cmp
---	"onsails/lspkind-nvim", -- vscode-like pictograms
---	"L3MON4D3/LuaSnip",
---	"hrsh7th/cmp-nvim-lsp",
---	"hrsh7th/cmp-buffer",
---	"hrsh7th/cmp-path",
---	"hrsh7th/nvim-cmp",
---	"hrsh7th/cmp-nvim-lsp-signature-help",
---	"yutkat/cmp-mocword",
---	"hrsh7th/cmp-cmdline",
---	"ray-x/cmp-treesitter",
---	-- - noice
---	"folke/noice.nvim",
---	"MunifTanjim/nui.nvim",
---	"folke/trouble.nvim",
---	"rcarriga/nvim-notify",
---	-- template
---	"mattn/vim-sonictemplate",
---	-- markdown
---	{
---		"iamcco/markdown-preview.nvim",
---		build = "cd app && npm install",
---		init = function()
---			vim.g.mkdp_filetypes = { "markdown" }
---		end,
---		ft = { "markdown" },
---	},
---	"mattn/vim-maketable",
---	--- obsidian
---	"epwalsh/obsidian.nvim",
---	"BurntSushi/ripgrep",
---	-- mark visualization
---	"chentoast/marks.nvim",
---	-- copilot
---	{ "zbirenbaum/copilot.lua", build = ":Copilot auth" },
---	{
---		"zbirenbaum/copilot-cmp",
---		dependencies = "copilot.lua",
---	},
---	-- terminal setting
---	"uga-rosa/ugaterm.nvim",
---	-- sidebar
---	"sidebar-nvim/sidebar.nvim",
---	-- neogit
---	"TimUntersberger/neogit",
---})
 
 vim.cmd([[colorscheme tokyonight]])
