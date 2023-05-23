d0b0e150c2d0c96bc3de27dd7e53cdb1a32c16d9
keimoriyama <keischwiiz@gmail.com>
2023-05-23 20:41:55 +0900

    fix configuration

--- .config/nvim/after/plugin/neogit.rc.lua
+++ .config/nvim/after/plugin/neogit.rc.lua
@@ -1,5 +1,5 @@
 local neogit = require("neogit")
 
-neogit.setup({ disable_commit_configuration = true })
+neogit.setup({ disable_commit_confirmation = true })
 local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "<Leader>g", "<cmd>Neogit<CR>", opts)
