a96623d193a695a57bb417694d9ab0258bd0a298
keimoriyama <keischwiiz@gmail.com>
2023-05-24 08:48:28 +0900

    add commit

--- .config/nvim/after/plugin/neogit.rc.lua
+++ .config/nvim/after/plugin/neogit.rc.lua
@@ -2,4 +2,4 @@ local neogit = require("neogit")
 
 neogit.setup({ disable_commit_confirmation = true })
 local opts = { noremap = true, silent = true }
-vim.keymap.set("n", "<Leader>g", "<cmd>Neogit<CR>", opts)
+vim.keymap.set("n", "<C-g>", "<cmd>Neogit<CR>", opts)
