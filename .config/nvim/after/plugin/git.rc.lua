9801ca36a7ba59b135cd7ab9775f9be874e06094
keimoriyama <keischwiiz@gmail.com>
2023-05-25 10:27:01 +0900

    delete git keymap

--- .config/nvim/after/plugin/git.rc.lua
+++ .config/nvim/after/plugin/git.rc.lua
@@ -17,21 +17,3 @@ git.setup({
 		browse = "<Leader>go",
 	},
 })
-
-local option = { noremap = true, silent = true }
-
-local file = ""
-local command_add = ""
-
-vim.api.nvim_create_autocmd("BufEnter", {
-	callback = function()
-		file = vim.api.nvim_buf_get_name(0)
-		command_add = string.format("<cmd>Git add %s<CR>", file)
-		vim.api.nvim_set_keymap("n", "<Leader>ga", command_add, option)
-	end,
-})
-
-vim.api.nvim_set_keymap("n", "<Leader>gc", "<cmd>Git commit<CR>", option)
-vim.api.nvim_set_keymap("n", "<Leader>gs", "<cmd>Git status<CR>", option)
-vim.api.nvim_set_keymap("n", "<Leader>gp", "<cmd>Git push<CR>", option)
-vim.api.nvim_set_keymap("n", "<Leader>gP", "<cmd>Git pull<CR>", option)
