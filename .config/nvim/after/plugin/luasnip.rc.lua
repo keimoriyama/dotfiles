64422686107d7450c553ee61a5b205fdf28ed1a3
keimoriyama <keischwiiz@gmail.com>
2023-05-28 23:29:39 +0900

    add luasnip conf

---
+++ .config/nvim/after/plugin/luasnip.rc.lua
@@ -0,0 +1,22 @@
+local status, luasnip = pcall(require, 'luasnip')
+if not status then return end
+
+-- Every unspecified option will be set to the default.
+ls.config.set_config({
+	history = true,
+	-- Update more often, :h events for more info.
+	updateevents = "TextChanged,TextChangedI",
+	ext_opts = {
+		[types.choiceNode] = {
+			active = {
+				virt_text = { { "choiceNode", "Comment" } },
+			},
+		},
+	},
+	-- treesitter-hl has 100, use something higher (default is 200).
+	ext_base_prio = 300,
+	-- minimal increase in priority.
+	ext_prio_increase = 1,
+	enable_autosnippets = true,
+})
+
