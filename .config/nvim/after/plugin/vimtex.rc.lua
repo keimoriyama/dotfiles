69d067e304a4dc835acb92851ce7df25498cad4b
keimoriyama <keischwiiz@gmail.com>
2023-06-06 23:14:43 +0900

    update vimtex settings

--- .config/nvim/after/plugin/vimtex.rc.lua
+++ .config/nvim/after/plugin/vimtex.rc.lua
@@ -1,7 +1,7 @@
 vim.g.vimtex_view_method = "skim"
 vim.g.vimtex_view_general_viewer = "skim"
 vim.g.vimtex_compiler_method = "latexmk"
-vim.g.vimtex_view_compiler_latexmk_engines = {"-", "-pdf"}
+vim.g.vimtex_view_compiler_latexmk_engines = { "-", "-pdf" }
 vim.g.latex_latexmk_options = "-pdf"
 vim.g.vimtex_syntax_enabled = 0
 
@@ -18,6 +18,13 @@ let g:vimtex_compiler_latexmk = {
             \   '-interaction=nonstopmode',
             \ ],
             \}
+]])
+
+vim.cmd([[
+let g:vimtex_quickfix_ignore_filters = [
+      \ 'Underfull',
+      \ 'Overfull',
+      \]
 ]])
 
 vim.cmd('let maplocalleader = " "')
