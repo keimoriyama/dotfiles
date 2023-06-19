84c53a64cb9c3aa28c227a0edcd9d7afbdbb80a7
keimoriyama <keischwiiz@gmail.com>
2023-06-19 16:59:13 +0900

    back up

--- .config/nvim/after/plugin/vimtex.rc.lua
+++ .config/nvim/after/plugin/vimtex.rc.lua
@@ -2,24 +2,40 @@ vim.g.vimtex_view_method = "skim"
 vim.g.vimtex_view_general_viewer = "skim"
 vim.g.vimtex_compiler_method = "latexmk"
 vim.g.vimtex_view_compiler_latexmk_engines = { "-", "-pdf" }
+vim.cmd([[
+let g:vimtex_compiler_latexmk_engines = {'_': '-pdfdvi'}
+]])
 vim.g.latex_latexmk_options = "-pdf"
+--
 vim.g.vimtex_syntax_enabled = 0
 
 vim.cmd([[
-let g:vimtex_compiler_latexmk = {
-            \ 'background' : 0,
-            \ 'build_dir' : '',
-            \ 'continuous' : 1,
-            \ 'options' : [
-            \   '-pdf',
-            \   '-verbose',
-            \   '-file-line-error',
-            \   '-synctex=1',
-            \   '-interaction=nonstopmode',
+ let g:vimtex_compiler_latexmk = {
+             \ 'background' : 0,
+             \ 'build_dir' : '',
+             \ 'continuous' : 1,
+             \ 'options' : [
+			 \ '-pdf',
+             \   '-verbose',
+             \   '-file-line-error',
+             \   '-synctex=1',
+             \   '-interaction=nonstopmode',
             \ ],
-            \}
-]])
-
+             \}
+ ]])
+-- vim.cmd([[let g:vimtex_compiler_latexmk = {
+--       \ 'background': 1,
+--       \ 'build_dir': '',
+--       \ 'continuous': 1,
+--       \ 'options': [
+--       \    '-pdfdvi',
+--       \    '-verbose',
+--       \    '-file-line-error',
+--       \    '-synctex=1',
+--       \    '-interaction=nonstopmode',
+--       \],
+--       \}]])
+--
 vim.cmd([[
 let g:vimtex_quickfix_ignore_filters = [
       \ 'Underfull',
