9ed94e0d3e603c5bece828b0728cc15e62da6008
keimoriyama <keischwiiz@gmail.com>
2023-07-03 17:11:29 +0900

    changed colorscheme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -50,7 +50,7 @@ local hyperlink_rules = {
 return {
 	font = wezterm.font_with_fallback({ "Hack Nerd Font", "Source Han Code JP" }),
 	font_size = 22,
-	color_scheme = "Kanagawa (Gogh)",
+	color_scheme = "tokyonight",
 	hyperlink_rules = hyperlink_rules,
 	hide_tab_bar_if_only_one_tab = true,
 	default_cursor_style = "SteadyUnderline",
