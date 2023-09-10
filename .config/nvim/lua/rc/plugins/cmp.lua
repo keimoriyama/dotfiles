2f86648377a1ca5ab3c661980b3653c1ab5f3079
keimoriyama <keischwiiz@gmail.com>
2023-09-10 21:48:03 +0900

    fix config

--- .config/nvim/lua/rc/plugins/cmp.lua
+++ .config/nvim/lua/rc/plugins/cmp.lua
@@ -1,7 +1,6 @@
 local spec = {
 	{
 		"hrsh7th/nvim-cmp",
-		event = { "InsertEnter", "CmdlineEnter" },
 		dependencies = {
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-buffer",
--- .config/nvim/lua/rc/plugins/sonic-tmeplate.lua
+++ .config/nvim/lua/rc/plugins/sonic-tmeplate.lua
@@ -1,12 +1,8 @@
 local spec = {
 	{
 		"mattn/vim-sonictemplate",
-		cmd = "Template",
 		config = function()
 			vim.g.sonictemplate_vim_template_dir = "~/.dotfiles/template"
-			vim.g.sonictemplate_key = 0
-			vim.g.sonictemplate_intelligent_key = 0
-			vim.g.sonictemplate_postfix_key = 0
 		end,
 	},
 }
