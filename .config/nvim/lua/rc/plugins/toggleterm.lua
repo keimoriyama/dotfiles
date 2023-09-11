7631b44adc8d804d6b2f96ca026e06842405b6b8
keimoriyama <keischwiiz@gmail.com>
2023-09-11 17:41:18 +0900

    add ugaterm

--- .config/nvim/lua/rc/plugins/toggleterm.lua
+++ .config/nvim/lua/rc/plugins/toggleterm.lua
@@ -1,19 +1,19 @@
 local spec = {
-	{
-		"akinsho/toggleterm.nvim",
-		config = function()
-			local status, toggleterm = pcall(require, "toggleterm")
-
-			if not status then
-				return
-			end
-
-			toggleterm.setup({})
-
-			local option = { noremap = true, silent = true }
-			vim.keymap.set({ "n", "t" }, "<C-t>", "<cmd>ToggleTerm<CR>", option)
-		end,
-	},
+	-- {
+	-- 	"akinsho/toggleterm.nvim",
+	-- 	config = function()
+	-- 		local status, toggleterm = pcall(require, "toggleterm")
+	--
+	-- 		if not status then
+	-- 			return
+	-- 		end
+	--
+	-- 		toggleterm.setup({})
+	--
+	-- 		local option = { noremap = true, silent = true }
+	-- 		-- vim.keymap.set({ "n", "t" }, "<C-t>", "<cmd>ToggleTerm<CR>", option)
+	-- 	end,
+	-- },
 }
 
 return spec
---
+++ .config/nvim/lua/rc/plugins/ugaterm.lua
@@ -0,0 +1,9 @@
+local spec = {
+	'uga-rosa/ugaterm.nvim',
+	config = function()
+		local option = { noremap = true, silent = true }
+		vim.keymap.set({ "n", "t" }, "<C-t>", "<cmd>UgatermOpen -toggle<cr>", option)
+	end
+}
+
+return spec
