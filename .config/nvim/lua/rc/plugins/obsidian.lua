fa2c903a042a92a393ab57e04eb1c4573f521e23
keimoriyama <keischwiiz@gmail.com>
2023-09-12 10:22:55 +0900

    add command

--- .config/nvim/lua/rc/plugins/obsidian.lua
+++ .config/nvim/lua/rc/plugins/obsidian.lua
@@ -25,6 +25,7 @@ local spec = {
 			local opts = { noremap = true, silent = true }
 			vim.keymap.set("n", "<leader>nn", "<cmd>ObsidianToday<cr>", opts)
 			vim.keymap.set("n", "gf", "<cmd>ObsidianFollowLink<CR>", opts)
+			vim.keymap.set("n", "<leader>os", "<cmd>ObsidianSearch<cr>", opts)
 		end,
 	},
 }
