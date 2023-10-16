9f5f7c88dea45257f1309c8b070cef16330e2860
keimoriyama <keischwiiz@gmail.com>
2023-10-16 13:18:01 +0900

    add dap config

--- .config/nvim/lua/rc/plugins/dap.lua
+++ .config/nvim/lua/rc/plugins/dap.lua
@@ -1,6 +1,83 @@
+local function rust_dap_config()
+	local dap = require("dap")
+	dap.configurations.rust = {
+		{
+			name = 'Rust Debug',
+			type = 'codelldb',
+			request = 'launch',
+			program = function()
+				return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+			end,
+			cwd = '${workspaceFolder}',
+			stopOnEntry = false,
+			args = {}
+		}
+	}
+end
+
+local function python_dap_config()
+	local dap = require("dap")
+	dap.adapters.python = function(cb, config)
+		if config.request == 'attach' then
+			---@diagnostic disable-next-line: undefined-field
+			local port = (config.connect or config).port
+			---@diagnostic disable-next-line: undefined-field
+			local host = (config.connect or config).host or '127.0.0.1'
+			cb({
+				type = 'server',
+				port = assert(port, '`connect.port` is required for a python `attach` configuration'),
+				host = host,
+				options = {
+					source_filetype = 'python',
+				},
+			})
+		else
+			cb({
+				type = 'executable',
+				command = 'path/to/virtualenvs/debugpy/bin/python',
+				args = { '-m', 'debugpy.adapter' },
+				options = {
+					source_filetype = 'python',
+				},
+			})
+		end
+	end
+end
 ---@type LazySpec
 local spec = {
-	{ "mfussenegger/nvim-dap" },
+	{
+		"mfussenegger/nvim-dap",
+		config = function()
+			rust_dap_config()
+			python_dap_config()
+			-- keymapの設定
+			-- TODO:見直す
+			vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
+			vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
+			vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
+			vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
+			vim.keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
+			vim.keymap.set('n', '<Leader>B', function() require('dap').set_breakpoint() end)
+			vim.keymap.set('n', '<Leader>lp',
+				function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
+			vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
+			vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
+			vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function()
+				require('dap.ui.widgets').hover()
+			end)
+			vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function()
+				require('dap.ui.widgets').preview()
+			end)
+			vim.keymap.set('n', '<Leader>df', function()
+				local widgets = require('dap.ui.widgets')
+				widgets.centered_float(widgets.frames)
+			end)
+			vim.keymap.set('n', '<Leader>ds', function()
+				local widgets = require('dap.ui.widgets')
+				widgets.centered_float(widgets.scopes)
+			end)
+		end
+	},
 	{ "jay-babu/mason-nvim-dap.nvim" },
 	{
 		"rcarriga/nvim-dap-ui",
@@ -36,20 +113,6 @@ local spec = {
 			})
 		end
 	},
-	{
-		"mfussenegger/nvim-dap-python",
-		ft = { 'python' },
-		config = function()
-			local dap_python = require('dap-python')
-			dap_python.setup("~/.virtualenvs/debugpy/bin/python")
-			dap_python.test_runner = 'pytest'
-
-			local opt = { noremap = true, silent = true }
-			vim.keymap.set("n", "<leader>dn", "<cmd>lua require('dap-python').test_method()<cr>", opt)
-			vim.keymap.set("n", "<leader>df", "<cmd>lua require('dap-python').test_class()<cr>", opt)
-			vim.keymap.set("n", "<leader>ds", "<ESC><cmd>lua require('dap-python').debug_selection()<CR>", opt)
-		end
-	}
 }
 
 return spec
