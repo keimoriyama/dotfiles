97d96294b59bf838cea56c6c94fe13ecd9c591c2
keimoriyama <keischwiiz@gmail.com>
2023-10-25 22:08:45 +0900

    bug fix

--- .config/nvim/denops/dpp_config.ts
+++ .config/nvim/denops/dpp_config.ts
@@ -5,6 +5,7 @@ import {
   Plugin,
 } from "https://deno.land/x/dpp_vim@v0.0.3/types.ts";
 import { Denops, fn } from "https://deno.land/x/dpp_vim@v0.0.3/deps.ts";
+import { getcwd } from "https://deno.land/x/denops_std@v5.0.1/function/mod.ts";
 
 type Toml = {
   hooks_file?: string;
@@ -25,6 +26,8 @@ export class Config extends BaseConfig {
     const [context, options] = await args.contextBuilder.get(args.denops);
 
     const tomls: Toml[] = [];
+    const base_dir = await getcwd(args.denops);
+    const toml_path = base_dir + "/rc/dpp.toml";
     tomls.push(
       await args.dpp.extAction(
         args.denops,
@@ -33,7 +36,7 @@ export class Config extends BaseConfig {
         "toml",
         "load",
         {
-          path: "./dpp.toml",
+          path: toml_path,
           options: {
             lazy: false,
           },
@@ -44,7 +47,10 @@ export class Config extends BaseConfig {
     const recordPlugins: Record<string, Plugin> = {};
     const ftplugins: Record<string, string> = {};
     const hooksFiles: string[] = [];
-    for (const toml in tomls) {
+    for (const toml of tomls) {
+      console.log(toml);
+    }
+    for (const toml of tomls) {
       for (const plugin of toml.plugins) {
         recordPlugins[plugin.name] = plugin;
       }
@@ -61,5 +67,29 @@ export class Config extends BaseConfig {
         hooksFiles.push(toml.hooks_file);
       }
     }
+    const lazyResult = await args.dpp.extAction(
+      args.denops,
+      context,
+      options,
+      "lazy",
+      "makeState",
+      {
+        plugins: Object.values(recordPlugins),
+      },
+    ) as LazyMakeStateResult;
+
+    return {
+      checkFiles: await fn.globpath(
+        args.denops,
+        base_dir + "/rc",
+        "*",
+        1,
+        1,
+      ) as unknown as string[],
+      ftplugins,
+      hooksFiles,
+      plugins: lazyResult.plugins,
+      stateLines: lazyResult.stateLines,
+    };
   }
 }
--- .config/nvim/lua/dpp.lua
+++ .config/nvim/lua/dpp.lua
@@ -1,31 +1,36 @@
-local cache = vim.fn.expand("~/.cache")
-local plugins = { 'Shougo/dpp.vim', 'Shougo/dpp-ext-toml', 'Shougo/dpp-ext-installer', 'denops/denops.vim' }
-for _, plugin in ipairs(plugins) do
-	local dir = cache .. '/dpp/repos/github.com/' .. plugin
-	if vim.fn.isdirectory(dir) == 0 then
-		print(dir)
+local cache = vim.fn.expand("~/.cache/dpp")
+local function InitPlugin(plugin)
+	local dir = cache .. '/repos/github.com/' .. plugin
+	if vim.fn.isdirectory(dir) ~= 0 then
 		vim.fn.system({
 			"git",
 			"clone",
 			"https://github.com/" .. plugin .. ".git",
 			dir,
 		})
+		vim.opt.rtp:prepend(dir)
 	end
-	-- if string.find(plugin, "dpp.vim") ~= nil then
-	vim.opt.rtp:prepend(dir)
-	-- end
 end
 
 vim.opt.compatible = false
 
 -- set dpp source path
-local dpp_base = "~/.cache/dpp/"
+local dpp_base = vim.fn.expand("~/.cache/dpp/")
 -- set dpp runtime path
-local dpp_src = '~/.cache/dpp/repos/github.com/Shougo/dpp.vim'
-local denops_src = '~/.cache/dpp/repos/github.com/denops/denops.vim'
-
+local dpp_src = vim.fn.expand('~/.cache/dpp/repos/github.com/Shougo/dpp.vim')
+local denops_src = vim.fn.expand('~/.cache/dpp/repos/github.com/denops/denops.vim')
+local plugins = {
+	"Shougo/dpp.vim",
+	'Shougo/dpp-ext-toml',
+	"Shougo/dpp-ext-lazy",
+	'Shougo/dpp-ext-installer',
+	"Shougo/dpp-ext-lazy",
+	'Shougo/dpp-protocol-git',
+	'denops/denops.vim' }
+for _, plugin in ipairs(plugins) do
+	InitPlugin(plugin)
+end
 -- Set dpp runtime path (required)
--- print(vim.fn.getcwd() .. "/lua/dpp_config.ts")
 vim.opt.rtp:prepend(dpp_src)
 if vim.fn["dpp#min#load_state"](dpp_base) then
 	vim.opt.rtp:prepend(denops_src)
