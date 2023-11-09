1b46425a88b48da12bd0bcb2afd2ec4a5ac543de
keimoriyama <keischwiiz@gmail.com>
2023-11-09 17:55:27 +0900

    update

--- .config/nvim/lua/rc/plugins/copilot.lua
+++ .config/nvim/lua/rc/plugins/copilot.lua
@@ -4,20 +4,23 @@ local spec = {
 		"zbirenbaum/copilot.lua",
 		event = 'InsertEnter',
 		config = function()
-			require("copilot").setup({
-				panel = {
-					enabled = false
-				},
-				suggestion = {
-					enabled = true,
-					auto_trigger = true,
-					keymap = {
-						accept = "<C-j>",
+			local current_dir = vim.fn.getcwd()
+			if string.match(current_dir, "Atcoder") == nil then
+				require("copilot").setup({
+					panel = {
+						enabled = false
+					},
+					suggestion = {
+						enabled = true,
+						auto_trigger = true,
+						keymap = {
+							accept = "<C-j>",
 
+						}
 					}
-				}
-			})
-			vim.keymap.set("i", "<C-c><C-t>", "<cmd>Copilot toggle<cr>")
+				})
+				vim.keymap.set("i", "<C-c><C-t>", "<cmd>Copilot toggle<cr>")
+			end
 		end
 	}
 }
