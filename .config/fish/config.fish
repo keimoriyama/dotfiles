4bc9988860637a4020b3e7de4a9c4dde48fb6745
keimoriyama <keischwiiz@gmail.com>
2023-12-18 10:13:57 +0900

    update config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -46,3 +46,24 @@ fish_add_path $HOME/.local/nvim/bin
 
 set -gx PKG_CONFIG_PATH /opt/homebrew/opt/libpq/lib/pkgconfig
 source ~/.config/fish/env.fish
+
+function attach_tmux_session_if_needed
+    set ID (tmux list-sessions)
+    if test -z "$ID"
+        tmux new-session
+        return
+    end
+
+    set new_session "Create New Session" 
+    set ID (echo $ID\n$new_session | peco --on-cancel=error | cut -d: -f1)
+    if test "$ID" = "$new_session"
+        tmux new-session
+    else if test -n "$ID"
+        tmux attach-session -t "$ID"
+    end
+end
+
+if test -z $TMUX && status --is-login
+    attach_tmux_session_if_needed
+end
+
