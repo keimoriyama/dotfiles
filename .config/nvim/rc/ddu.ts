4104d90524813257680080ccc4c68647b49ea41a
keimoriyama <keischwiiz@gmail.com>
2023-12-22 14:48:16 +0900

    update latex env

--- .config/nvim/rc/ddu.ts
+++ .config/nvim/rc/ddu.ts
@@ -1,16 +1,7 @@
-import {
-  ActionArguments,
-  ActionFlags,
-  BaseConfig,
-  Ddu,
-} from "https://deno.land/x/ddu_vim@v3.7.0/types.ts";
-import { Denops, fn } from "https://deno.land/x/ddu_vim@v3.7.0/deps.ts";
+import { BaseConfig } from "https://deno.land/x/ddu_vim@v3.7.0/types.ts";
 import { ConfigArguments } from "https://deno.land/x/ddu_vim@v3.7.0/base/config.ts";
-import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.7.1/file.ts";
 import { Params as FfParams } from "https://deno.land/x/ddu_ui_ff@v1.1.0/ff.ts";
 import { Params as FilerParams } from "https://deno.land/x/ddu_ui_filer@v1.1.0/filer.ts";
-
-type Params = Record<string, unknown>;
 
 export class Config extends BaseConfig {
   override config(args: ConfigArguments): Promise<void> {
