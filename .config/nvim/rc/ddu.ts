9b1c0f05de817fcf30c83548694e262cb5bea49c
keimoriyama <keischwiiz@gmail.com>
2024-02-09 12:53:37 +0900

    upadte

--- .config/nvim/rc/ddu.ts
+++ .config/nvim/rc/ddu.ts
@@ -4,138 +4,138 @@ import { Params as FfParams } from "https://deno.land/x/ddu_ui_ff@v1.1.0/ff.ts";
 import { Params as FilerParams } from "https://deno.land/x/ddu_ui_filer@v1.1.0/filer.ts";
 
 export class Config extends BaseConfig {
-	override config(args: ConfigArguments): Promise<void> {
-		args.contextBuilder.patchGlobal({
-			ui: "ff",
-			uiParams: {
-				ff: {
-					autoAction: {
-						name: "preview",
-					},
-					startAutoAction: true,
-					split: "floating",
-					prompt: "> ",
-					floatingBorder: "rounded",
-					floatingTitle: "Ddu ff",
-					floatingTitlePos: "center",
-					highlights: {
-						floating: "Normal",
-						floatingBorder: "Normal",
-					},
-					filterFloatingPosition: "bottom",
-					previewFloating: true,
-					previewSplit: "horizontal",
-					previewFloatingBorder: "rounded",
-					previewFloatingTitle: "preview",
-					previewFloatingTitlePos: "center",
-				} as Partial<FfParams>,
-				filer: {
-					split: "floating",
-					floatingBorder: "rounded",
-					floatingTitle: "filer",
-					floatingTitlePos: "center",
-					toggle: true,
-					sort: "filename",
-				} as Partial<FilerParams>,
-			},
-			sourceOptions: {
-				_: {
-					matchers: ["matcher_substring", "matcher_kensaku"],
-					sorters: ["sorter_alpha"],
-				},
-				file_rec: {
-					matchers: ["matcher_substring"],
-					converters: ["converter_devicon"],
-				},
-			},
-			filterParams: {
-				matcher_substring: {
-					highlightMatched: "Title",
-				},
-				matcher_fzf: {
-					highlightMatched: "Search",
-				},
-				matcher_kensaku: {
-					highlightMatched: "Search",
-				},
-			},
-			kindOptions: {
-				file: {
-					defaultAction: "open",
-				},
-				help: {
-					defaultAction: "open",
-				},
-				rg: {
-					defaultAction: "open",
-				},
-				lsp: {
-					defaultAction: "open",
-				},
-			},
-		});
-		args.contextBuilder.patchLocal("filer", {
-			ui: "filer",
-			sources: {
-				name: "file",
-				params: {},
-			},
-			sourceOptions: {
-				_: {
-					columns: ["icon_filename"],
-					sorters: ["sorter_alpha"],
-				},
-			},
-			kindOptions: {
-				file: {
-					defaultAction: "open",
-				},
-			},
-			uiParams: {
-				filer: {
-					split: "floating",
-					floatingBorder: "rounded",
-					floatingTitle: "filer",
-					floatingTitlePos: "center",
-				},
-			},
-		});
+  override config(args: ConfigArguments): Promise<void> {
+    args.contextBuilder.patchGlobal({
+      ui: "ff",
+      uiParams: {
+        ff: {
+          autoAction: {
+            name: "preview",
+          },
+          startAutoAction: true,
+          split: "floating",
+          prompt: "> ",
+          floatingBorder: "rounded",
+          floatingTitle: "Ddu ff",
+          floatingTitlePos: "center",
+          highlights: {
+            floating: "Normal",
+            floatingBorder: "Normal",
+          },
+          filterFloatingPosition: "bottom",
+          previewFloating: true,
+          previewSplit: "horizontal",
+          previewFloatingBorder: "rounded",
+          previewFloatingTitle: "preview",
+          previewFloatingTitlePos: "center",
+        } as Partial<FfParams>,
+        filer: {
+          split: "floating",
+          floatingBorder: "rounded",
+          floatingTitle: "filer",
+          floatingTitlePos: "center",
+          toggle: true,
+          sort: "filename",
+        } as Partial<FilerParams>,
+      },
+      sourceOptions: {
+        _: {
+          matchers: ["matcher_substring", "matcher_kensaku"],
+          sorters: ["sorter_alpha"],
+        },
+        file_rec: {
+          matchers: ["matcher_substring"],
+          converters: ["converter_devicon"],
+        },
+      },
+      filterParams: {
+        matcher_substring: {
+          highlightMatched: "Title",
+        },
+        matcher_fzf: {
+          highlightMatched: "Search",
+        },
+        matcher_kensaku: {
+          highlightMatched: "Search",
+        },
+      },
+      kindOptions: {
+        file: {
+          defaultAction: "open",
+        },
+        help: {
+          defaultAction: "open",
+        },
+        rg: {
+          defaultAction: "open",
+        },
+        lsp: {
+          defaultAction: "open",
+        },
+      },
+    });
+    args.contextBuilder.patchLocal("filer", {
+      ui: "filer",
+      sources: {
+        name: "file",
+        params: {},
+      },
+      sourceOptions: {
+        _: {
+          columns: ["icon_filename"],
+          sorters: ["sorter_alpha"],
+        },
+      },
+      kindOptions: {
+        file: {
+          defaultAction: "open",
+        },
+      },
+      uiParams: {
+        filer: {
+          split: "floating",
+          floatingBorder: "rounded",
+          floatingTitle: "filer",
+          floatingTitlePos: "center",
+        },
+      },
+    });
 
-		args.contextBuilder.patchLocal("lsp:hierarchy", {
-			sourceOptions: {
-				_: {
-					converters: ["converter_lsp_symbol"],
-				},
-			},
-			uiParams: {
-				ff: {
-					displayTree: true,
-					startFilter: false,
-				},
-			},
-		});
+    args.contextBuilder.patchLocal("lsp:hierarchy", {
+      sourceOptions: {
+        _: {
+          converters: ["converter_lsp_symbol"],
+        },
+      },
+      uiParams: {
+        ff: {
+          displayTree: true,
+          startFilter: false,
+        },
+      },
+    });
 
-		args.contextBuilder.patchLocal("file_rec", {
-			sources: {
-				name: "file_rec",
-				params: {
-					ignoreDirectories: [
-						".git",
-						"node_modules",
-						"vendor",
-						".mypy_cache",
-						".hydra",
-						"mlruns",
-						".venv",
-						"logs",
-						"data",
-						"outputs",
-						"output",
-						"notebooks",
-					],
-				},
-			},
-		});
-		return Promise.resolve();
-	}
+    args.contextBuilder.patchLocal("file_rec", {
+      sources: {
+        name: "file_rec",
+        params: {
+          ignoreDirectories: [
+            ".git",
+            "node_modules",
+            "vendor",
+            ".mypy_cache",
+            ".hydra",
+            "mlruns",
+            ".venv",
+            "logs",
+            "data",
+            "outputs",
+            "output",
+            "notebooks",
+          ],
+        },
+      },
+    });
+    return Promise.resolve();
+  }
 }
--- .config/nvim/rc/hooks/ddu-ui-ff.lua
+++ .config/nvim/rc/hooks/ddu-ui-ff.lua
@@ -62,5 +62,4 @@ local function resize()
 end
 
 resize()
-vim.api.nvim_create_autocmd("VimResized", { callback = resize })
 -- }}}
