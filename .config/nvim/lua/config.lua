4a1beea391361a61c6fcbbc0cdf128edf0e0d4ed
keimoriyama <keischwiiz@gmail.com>
2024-02-18 12:23:16 +0900

    update

--- .config/nvim/lua/config.lua
+++ .config/nvim/lua/config.lua
@@ -28,18 +28,8 @@ vim.opt.shiftwidth = 4
 vim.opt.modifiable = true
 vim.opt.cmdheight = 0
 -- コピペの共通化
-vim.g.clipboard = {
-  name = 'tmux',
-  copy = {
-    ["+"] = { 'tmux', 'load-buffer', '-w', '-' },
-    ["*"] = { 'tmux', 'load-buffer', '-w', '-' },
-  },
-  paste = {
-    ["+"] = { 'tmux', 'save-buffer', '-' },
-    ["*"] = { 'tmux', 'save-buffer', '-' },
-  },
-  cache_enabled = false,
-}
+vim.opt.clipboard:append({ unnamedplus = true })
+
 vim.opt.scrolloff = 0
 if vim.fn.has("mac") == 1 then
 	vim.cmd([[set clipboard+=unnamed]])
@@ -47,8 +37,8 @@ else
 	vim.cmd([[set clipboard^=unnamedplus]])
 end
 
-local notify                    = vim.notify
-vim.notify                      = function(msg, ...)
+local notify = vim.notify
+vim.notify = function(msg, ...)
 	if msg:match("warning: multiple different client offset_encodings") then
 		return
 	end
@@ -56,25 +46,25 @@ vim.notify                      = function(msg, ...)
 end
 vim.g.loaded_man = 1
 vim.g.did_install_default_menus = 1
-vim.g.did_install_syntax_menu   = 1
-vim.g.did_indent_on             = 1
-vim.g.did_load_filetypes        = 1
-vim.g.did_load_ftplugin         = 1
-vim.g.loaded_2html_plugin       = 1
-vim.g.loaded_gzip               = 1
-vim.g.loaded_man                = 1
-vim.g.loaded_matchit            = 1
+vim.g.did_install_syntax_menu = 1
+vim.g.did_indent_on = 1
+vim.g.did_load_filetypes = 1
+vim.g.did_load_ftplugin = 1
+vim.g.loaded_2html_plugin = 1
+vim.g.loaded_gzip = 1
+vim.g.loaded_man = 1
+vim.g.loaded_matchit = 1
 vim.g.loaded_tar = 1
-vim.g.loaded_matchparen         = 1
-vim.g.loaded_netrwPlugin        = 1
-vim.g.loaded_remote_plugins     = 1
-vim.g.loaded_shada_plugin       = 1
-vim.g.loaded_spellfile_plugin   = 1
-vim.g.loaded_tarPlugin          = 1
-vim.g.loaded_tutor_mode_plugin  = 1
+vim.g.loaded_matchparen = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_remote_plugins = 1
+vim.g.loaded_shada_plugin = 1
+vim.g.loaded_spellfile_plugin = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_tutor_mode_plugin = 1
 vim.g.loaded_zip = 1
-vim.g.loaded_zipPlugin          = 1
-vim.g.skip_loading_mswin        = 1
+vim.g.loaded_zipPlugin = 1
+vim.g.skip_loading_mswin = 1
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_netrwSettings = 1
@@ -82,5 +72,5 @@ vim.g.loaded_netrwFileHandlers = 1
 vim.g.loaded_getscript = 1
 vim.g.loaded_getscriptPlugin = 1
 
-vim.g.python3_host_prog = '/opt/homebrew/bin/python3'
-vim.opt.mouse                   = ""
+vim.g.python3_host_prog = "/opt/homebrew/bin/python3"
+vim.opt.mouse = ""
