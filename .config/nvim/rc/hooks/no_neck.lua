fe8675c848ec3198115dcf51c1328986e26344cc
keimoriyama <keischwiiz@gmail.com>
2024-04-19 15:53:33 +0900

    update

--- .config/nvim/rc/hooks/no_neck.lua
+++ .config/nvim/rc/hooks/no_neck.lua
@@ -3,14 +3,14 @@ local width = vim.fn.winwidth(0)
 if width >= 100 then
 	require("no-neck-pain").enable()
 end
-vim.api.nvim_create_autocmd("VimResized", {
-	callback = function()
-		width = vim.fn.winwidth(0)
-		if width >= 100 then
-			require("no-neck-pain").enable()
-		else
-			require("no-neck-pain").disable()
-		end
-	end,
-})
+-- vim.api.nvim_create_autocmd("VimResized", {
+-- 	callback = function()
+-- 		width = vim.fn.winwidth(0)
+-- 		if width >= 100 then
+-- 			require("no-neck-pain").enable()
+-- 		else
+-- 			require("no-neck-pain").disable()
+-- 		end
+-- 	end,
+-- })
 -- }}}
