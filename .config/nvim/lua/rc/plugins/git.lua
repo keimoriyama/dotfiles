c6b3462d39ceeb108c1bcb85db7a7b16291d41ec
keimoriyama <keischwiiz@gmail.com>
2024-05-24 08:28:58 +0900

    update

--- .config/nvim/lua/rc/plugins/git.lua
+++ .config/nvim/lua/rc/plugins/git.lua
@@ -3,36 +3,36 @@ local M = {}
 local add, later = MiniDeps.add, MiniDeps.later
 
 function M.setup()
-	later(function()
-		add({ source = "lambdalisue/gin.vim", depends = { "vim-denops/denops.vim" } })
-
-		vim.keymap.set({ "n" }, "<leader>gs", "<Cmd>GinStatus<Cr>", {})
-		vim.keymap.set({ "n" }, "<leader>gl", "<Cmd>GinLog --graph --oneline<Cr>", {})
-		vim.api.nvim_create_autocmd({ "FileType" }, {
-			pattern = { "gin-diff", "gin-log", "gin-status" },
-			callback = function()
-				local keymap = vim.keymap.set
-				local opts = { buffer = true, noremap = true }
-				keymap({ "n" }, "c", "<Cmd>Gin commit<Cr>", opts)
-				keymap({ "n" }, "s", "<Cmd>GinStatus<Cr>", opts)
-				keymap({ "n" }, "L", "<Cmd>GinLog --graph --oneline<Cr>", opts)
-				keymap({ "n" }, "d", "<Cmd>GinDiff --cached<Cr>", opts)
-				keymap({ "n" }, "q", "<Cmd>bdelete<Cr>", opts)
-				keymap({ "n" }, "p", [[<Cmd>lua vim.notify("Gin push")<Cr><Cmd>Gin push<Cr>]], opts)
-				keymap({ "n" }, "P", [[<Cmd>lua vim.notify("Gin pull")<Cr><Cmd>Gin pull<Cr>]], opts)
-			end,
-		})
-
-		vim.api.nvim_create_autocmd({ "FileType" }, {
-			pattern = "gin-status",
-			callback = function()
-				local keymap = vim.keymap.set
-				local opts = { buffer = true, noremap = true }
-				keymap({ "n" }, "h", "<Plug>(gin-action-stage)", opts)
-				keymap({ "n" }, "l", "<Plug>(gin-action-unstage)", opts)
-			end,
-		})
-	end)
+	-- later(function()
+	-- 	add({ source = "lambdalisue/gin.vim", depends = { "vim-denops/denops.vim" } })
+	--
+	-- 	vim.keymap.set({ "n" }, "<leader>gs", "<Cmd>GinStatus<Cr>", {})
+	-- 	vim.keymap.set({ "n" }, "<leader>gl", "<Cmd>GinLog --graph --oneline<Cr>", {})
+	-- 	vim.api.nvim_create_autocmd({"FileType" }, {
+	-- 		pattern = { "gin-diff", "gin-log", "gin-status" },
+	-- 		callback = function()
+	-- 			local keymap = vim.keymap.set
+	-- 			local opts = { buffer = true, noremap = true }
+	-- 			keymap({ "n" }, "c", "<Cmd>Gin commit<Cr>", opts)
+	-- 			keymap({ "n" }, "s", "<Cmd>GinStatus<Cr>", opts)
+	-- 			keymap({ "n" }, "L", "<Cmd>GinLog --graph --oneline<Cr>", opts)
+	-- 			keymap({ "n" }, "d", "<Cmd>GinDiff --cached<Cr>", opts)
+	-- 			keymap({ "n" }, "q", "<Cmd>bdelete<Cr>", opts)
+	-- 			keymap({ "n" }, "p", [[<Cmd>lua vim.notify("Gin push")<Cr><Cmd>Gin push<Cr>]], opts)
+	-- 			keymap({ "n" }, "P", [[<Cmd>lua vim.notify("Gin pull")<Cr><Cmd>Gin pull<Cr>]], opts)
+	-- 		end,
+	-- 	})
+	--
+	-- 	vim.api.nvim_create_autocmd({ "FileType" }, {
+	-- 		pattern = "gin-status",
+	-- 		callback = function()
+	-- 			local keymap = vim.keymap.set
+	-- 			local opts = { buffer = true, noremap = true }
+	-- 			keymap({ "n" }, "h", "<Plug>(gin-action-stage)", opts)
+	-- 			keymap({ "n" }, "l", "<Plug>(gin-action-unstage)", opts)
+	-- 		end,
+	-- 	})
+	-- end)
 end
 
 return M
