3181c7ef73a20065d23670de3650d3f577127c3d
keimoriyama <keischwiiz@gmail.com>
2024-06-11 16:44:59 +0900

    update plugins

--- .config/nvim/rc/hooks/memolist.lua
+++ .config/nvim/rc/hooks/memolist.lua
@@ -1,5 +1,6 @@
 -- lua_source {{{
 vim.g.memolist_path = "$HOME/Documents/memo"
+vim.g.memolist_memo_suffix = "md"
 local opt = { noremap = true, silent = true }
 vim.keymap.set("n", "<leader>mn", "<cmd>MemoNew<CR>", opt)
 vim.keymap.set("n", "<leader>ml", "<cmd>MemoList<CR>", opt)
