884f288ad0403c6d8aebf246fa8764c432cafecf
keimoriyama <keischwiiz@gmail.com>
2024-07-05 15:23:42 +0900

    updaet

--- .config/nvim/rc/hooks/ddc.lua
+++ .config/nvim/rc/hooks/ddc.lua
@@ -1,6 +1,6 @@
 -- lua_add {{{
 vim.fn["pum#set_option"]({
-	item_orders = { "abbr", "space", "kind", "space", "menu" },
+	-- item_orders = { "abbr", "space", "kind", "space", "menu" },
 	highlight_selected = "CursorLine",
 	horizontal_menu = false,
 	offset_cmdcol = 0,
