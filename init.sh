aa43ae4cf3e3d6e75c503c6e0dedb117db32b949
keimoriyama <keischwiiz@gmail.com>
2024-07-25 14:13:03 +0900

    update

--- init.sh
+++ init.sh
@@ -4,3 +4,6 @@ for f in ${FILES[@]}; do
 	ln -snfv ~/.dotfiles/$f $HOME/$f
 	echo $f
 done
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew bundle
