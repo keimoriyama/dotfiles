66acd451f65c4fc27e715a16513ef127ea5e9766
kei <kei@keinombp.lan>
2024-07-27 16:52:37 +0900

    update some scripts

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 FILES=(.zshrc .zshenv .config .tmux.conf .tigrc .vimrc .hammerspoon .p10k.zsh)
 # 移動できたらリンクを実行する
 for f in ${FILES[@]}; do
-	ln -snfv ~/.dotfiles/$f $HOME/$f
+	ln -snfv $HOME/.dotfiles/$f $HOME/$f
 	echo $f
 done
 
@@ -10,4 +10,5 @@ done
 eval "$(/opt/homebrew/bin/brew shellenv)"
 brew bundle
 
+ln -snfv $HOME/.dotfiles/.config/aquaskk/keymap.conf $HOME/Library/Application\ Support/AquaSKK/keymap.conf
 
