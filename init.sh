972d51805477b165aa686b0dddeb67e612eed8d7
kei <kei@keinoMacBook-Pro.local>
2024-08-11 20:58:57 +0900

    udpate

--- init.sh
+++ init.sh
@@ -5,19 +5,19 @@ for f in ${FILES[@]}; do
 	echo $f
 done
 
-if [["$OSTYPE" == 'darwin'*]]; then 
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-	(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/kei/.zprofile
-	eval "$(/opt/homebrew/bin/brew shellenv)"
-	brew bundle
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-	ln -snfv $HOME/.dotfiles/.config/aquaskk/keymap.conf $HOME/Library/Application\ Support/AquaSKK/keymap.conf
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/kei/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+brew bundle
+
+ln -snfv $HOME/.dotfiles/.config/aquaskk/keymap.conf $HOME/Library/Application\ Support/AquaSKK/keymap.conf
 
 
-	curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
-	fisher install jethrokuan/z
-	fisher install oh-my-fish/plugin-peco
-	fisher install IlanCosman/tide@v6
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+fisher install jethrokuan/z
+fisher install oh-my-fish/plugin-peco
+fisher install IlanCosman/tide@v6
 
-	cargo install mocword
-fi
+cargo install mocword
+
