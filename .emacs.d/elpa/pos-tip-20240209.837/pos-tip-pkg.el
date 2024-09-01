0dc44b5c7731471b14daca307d10d12b285daee4
kei <keischwiiz@gmail.com>
2024-09-01 14:50:28 +0900

    update

--- .emacs.d/elpa/pos-tip-20240209.837/pos-tip-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "pos-tip" "20240209.837" "Show tooltip at point" 'nil :commit "4889e08cf9077c8589ea6fea4e2ce558614dfcde" :keywords
-  '("tooltip"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
