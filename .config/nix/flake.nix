868c7cc10e486a08262fe4fdede23b5ad7d5cb99
kei <keischwiiz@gmail.com>
2025-03-19 15:23:51 +0900

    update configs

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -1,43 +1,19 @@
 {
-  description = "Example nix-darwin system flake";
+  description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin/master";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+   neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.vim
-        ];
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#keinoMacBook-Pro
-    darwinConfigurations."keinoMacBook-Pro" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
-    };
+  outputs = { self, nixpkgs, neovim-nightly-overlay }: {
+	packages.aarch64-darwin.my-packages = nixpkgs.legacyPackages.aarch64-darwin.buildEnv {
+   name="my-packages";
+		paths = [
+		nixpkgs.legacyPackages.aarch64-darwin.git
+        nixpkgs.legacyPackages.aarch64-darwin.curl
+		neovim-nightly-overlay.packages.aarch64-darwin.neovim
+		];
+	};
   };
 }
