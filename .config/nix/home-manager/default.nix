0d2db7de48f4068a7f2af3ada3d3e6bdea030cec
keimoriyama <keischwiiz@gmail.com>
2025-05-29 16:37:13 +0900

    Update Emacs configuration
    Updated emacs overlay to latest revision.
    
    Removed ollama, slack and spotify from default programs.
    Added pretty-hydra to emacs packages.

--- .config/nix/home-manager/default.nix
+++ .config/nix/home-manager/default.nix
@@ -80,10 +80,10 @@ in {
       nvfetcher
       udev-gothic
       ghostscript
-      ollama
-      slack
+      # ollama
+      # slack
       wezterm
-      spotify
+      # spotify
       docker
     ];
     file = {
