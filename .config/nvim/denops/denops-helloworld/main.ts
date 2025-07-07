4ddfbc4284cc6e9fd4745daa9f1dfbd3dc30a609
keimoriyama <keischwiiz@gmail.com>
2025-07-07 21:30:58 +0900

    wip markdown previewer

--- .config/nvim/denops/denops-helloworld/main.ts
+++ .config/nvim/denops/denops-helloworld/main.ts
@@ -1,5 +1,6 @@
 import type { Entrypoint } from "jsr:@denops/std@^7.0.0";
 import { assert, is } from "jsr:@core/unknownutil@^4.3.0";
+import * as gfm from "jsr:@deno/gfm"
 
 export const main: Entrypoint = (denops) => {
   denops.dispatcher = {
--- .config/nvim/denops/denops-markdown-previewer/markdown2html.ts
+++ .config/nvim/denops/denops-markdown-previewer/markdown2html.ts
@@ -1,28 +1,29 @@
 import { CSS, KATEX_CSS, render } from "jsr:@deno/gfm";
 // import { Denops } from "jsr:@denops/std@^7.0.0";
+// import * as gfm from "jsr:@deno/gfm"
 
 export function markdown2html(markdown: string) {
   console.log(markdown);
-  const body = render(markdown);
-  return `
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-      main {
-        max-width: 800px;
-        margin: 0 auto;
-      }
-      ${CSS}
-    </style>
-  </head>
-  <body>
-    <main data-color-mode="light" data-light-theme="light" data-dark-theme="dark" class="markdown-body">
-      ${body}
-    </main>
-  </body>
-</html>
-`;
+//   const body =render(markdown);
+//   return `
+// <!DOCTYPE html>
+// <html lang="en">
+//   <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <style>
+//       main {
+//         max-width: 800px;
+//         margin: 0 auto;
+//       }
+//       ${CSS}
+//     </style>
+//   </head>
+//   <body>
+//     <main data-color-mode="light" data-light-theme="light" data-dark-theme="dark" class="markdown-body">
+//       ${body}
+//     </main>
+//   </body>
+// </html>
+// `;
 }
