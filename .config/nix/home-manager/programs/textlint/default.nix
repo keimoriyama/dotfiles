9b3385aa02d15db20679283ae070a0fb9498850c
keimoriyama <kei_moriyama@icloud.com>
2025-09-22 10:44:37 +0900

    add textlint config

---
+++ .config/nix/home-manager/programs/textlint/default.nix
@@ -0,0 +1,13 @@
+{pkgs}: {
+  home = {
+    file = {
+      ".textlintrc".source = ./textlintrc;
+    };
+    packages = with pkgs; [
+      textlint
+      textlint-plugin-org
+      textlint-plugin-latex2e
+      textlint-rule-preset-ja-technical-writing
+    ];
+  };
+}
