fdcf91e8bd088c6fe7e8f4326344ba58ebdd9052
keimoriyama <kei_moriyama@icloud.com>
2025-12-02 23:14:03 +0900

    update

--- home-manager/programs/neovim/nvim/lua/treesitter.lua
+++ home-manager/programs/neovim/nvim/lua/treesitter.lua
@@ -1,33 +1,8 @@
 -- lua_source {{{
-require("nvim-treesitter.configs").setup({
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = { "markdown" },
-	},
-	indent = { enable = false, disable = { "python" } },
-	ensure_installed = {
-		"tsx",
-		"toml",
-		"gitignore",
-		"json",
-		"yaml",
-		"css",
-		"html",
-		"lua",
-		"python",
-		"cpp",
-		"bash",
-		"vimdoc",
-		"markdown",
-		"markdown_inline",
-		"latex",
-	},
-	auto_install = true,
-	autotag = { enable = true },
-	yati = { enable = true, indent = { enable = false } },
-})
+require("nvim-treesitter").setup({})
 
-local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
+pcall(vim.treesitter.start)
+-- local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+-- parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
 vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 -- }}}
