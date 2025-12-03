80ed1485803566118ac6eded58748b788a0dfb79
keimoriyama <kei_moriyama@icloud.com>
2025-12-03 12:25:34 +0900

    add custom query

--- home-manager/programs/neovim/nvim/lua/treesitter.lua
+++ home-manager/programs/neovim/nvim/lua/treesitter.lua
@@ -1,8 +1,21 @@
 -- lua_source {{{
-require("nvim-treesitter").setup({})
+local treesitter = require("nvim-treesitter")
+treesitter.setup({
+	install_dir = vim.fs.joinpath(vim.fn.stdpath("data"), "site"),
+})
 
-pcall(vim.treesitter.start)
--- local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
--- parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
-vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+local installed_parser = { "c", "lua", "vim", "vimdoc", "python", "toml", "json" }
+treesitter.install(installed_parser, {})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = installed_parser,
+	callback = function()
+		-- syntax highlighting, provided by Neovim
+		vim.treesitter.start()
+		-- folds, provided by Neovim
+		vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+		-- indentation, provided by nvim-treesitter
+		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+	end,
+})
 -- }}}
