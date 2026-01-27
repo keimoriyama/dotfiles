724f9a1e7f19823647a2a8a70712070e4e353c7d
keimoriyama <kei_moriyama@icloud.com>
2026-01-27 16:14:19 +0900

    update

--- cargo-compete/default.nix
+++ cargo-compete/default.nix
@@ -6,9 +6,10 @@ pkgs.rustPlatform.buildRustPackage {
   name = "carg-compete";
   src = sources.cargo-compete.src;
   # cargoLock = sources.cargo-compete.cargoLock."Cargo.lock";
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-  };
+  # cargoLock = {
+  #   lockFile = ./Cargo.lock;
+  # };
+	cargoHash="sha256-lid1tyR8Y6lvjpeGJ4vGzqDTY6V2y/5rL9fGyjyF3yw=";
   doCheck = false;
   nativeBuildInputs = [
     pkgs.pkg-config
--- yaskkserv2/default.nix
+++ yaskkserv2/default.nix
@@ -6,11 +6,7 @@ pkgs.rustPlatform.buildRustPackage {
   name = "yaskkserv2";
   src = sources.yaskkserv2.src;
   cargBuildFlags = ["--release"];
-  # cargoLock = sources.yaskkserv2.cargoLock."Cargo.lock";
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-  };
-	cargoHash = "";
+  cargoHash = "sha256-cycs8Zism228rjMaBpNYa4K1Ll760UhLKkoTX6VJRU0=";
   meta = with pkgs.lib; {
     description = "A Japanese input method server compatible with SKK";
   };
