37404d28792565e06840a4df1585593c9b3c1dcf
keimoriyama <kei_moriyama@icloud.com>
2026-01-31 12:31:29 +0900

    update config

--- home-manager/packages/emacs/default.nix
+++ home-manager/packages/emacs/default.nix
@@ -8,10 +8,23 @@
       preBuild = null;
     });
   };
+  parallelBuildAttrs = {
+    enableParallelBuilding = true;
+    env = {
+      NATIVE_COMP_JOBS = "0";
+      LSP_USE_PLISTS = "1";
+    };
+  };
 in {
   emacs-stable = pkgs.emacsWithPackagesFromUsePackage {
     config = builtins.toFile "empty.el" "";
-    package = pkgs.emacs;
+    package = pkgs.emacs.overrideAttrs (old:
+      parallelBuildAttrs
+      // {
+        buildInputs = old.buildInputs ++ lib.optional pkgs.stdenv.isDarwin [pkgs.apple-sdk];
+        configureFlags = old.configureFlags ++ ["--with-xwidgets"];
+        env = (old.env or {}) // parallelBuildAttrs.env;
+      });
     extraEmacsPackages = import ./epkgs.nix {inherit pkgs sources;};
     override = override;
   };
--- home-manager/programs/emacs/early-init.el
+++ home-manager/programs/emacs/early-init.el
@@ -1,2 +1,2 @@
 (setq debug-on-error t)
-(setenv "LSP_USE_PLISTS" "true")
+;;(setenv "LSP_USE_PLISTS" "true")
