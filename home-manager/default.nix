cd7bc7e0ce02ab15094553f31515414ec8fb0157
keimoriyama <kei_moriyama@icloud.com>
2026-03-07 09:47:53 +0900

    add steam

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -157,6 +157,13 @@ in {
         brewCasks.chatgpt
         brewCasks.ollama-app
         brewCasks.alt-tab
+        (brewCasks.steam.overrideAttrs
+          (oldAttrs: {
+            src = pkgs.fetchurl {
+              url = builtins.head oldAttrs.src.urls;
+              hash = "sha256-X1VnDJGv02A6ihDYKhedqQdE/KmPAQZkeJHudA6oS6M=";
+            };
+          }))
       ];
     file = {
       ".skk-dict/SKK-JISYO.L".source = "${pkgs.skkDictionaries.l}/share/skk/SKK-JISYO.L";
