7e19502021d7a26ce7938f6f72f01a1865aa702f
keimoriyama <keimoriy4ma@gmail.com>
2026-04-03 21:52:24 +0900

    bug fix

--- hosts/nixos-wsl/default.nix
+++ hosts/nixos-wsl/default.nix
@@ -23,6 +23,9 @@
     };
   };
 
+  # Disable hardware graphics (not needed for WSL)
+  hardware.opengl.enable = lib.mkForce false;
+
   # Set your time zone
   time.timeZone = "America/Los_Angeles";
 
