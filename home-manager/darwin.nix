d718703ed0f704e950e49ffb834ff651260f14a7
keimoriyama <keimoriy4ma@gmail.com>
2026-07-02 11:09:48 -0700

    update darwin.nix

--- home-manager/darwin.nix
+++ home-manager/darwin.nix
@@ -2,17 +2,10 @@
 with pkgs; [
   brewCasks.zoom
   brewCasks.skim
-  brewCasks.docker-desktop
+  # brewCasks.docker-desktop
   brewCasks.chatgpt
   # brewCasks.ollama-app
-  # brewCasks.alt-tab
   brewCasks.codex-app
+  brewCasks.claude
   # brewCasks.notion-calendar
-  # (brewCasks.steam.overrideAttrs
-  #   (oldAttrs: {
-  #     src = pkgs.fetchurl {
-  #       url = builtins.head oldAttrs.src.urls;
-  #       hash = "sha256-4av7qqe+Pg9IoODUwxMjPgWGGx0mrzKDDdyDi+iPJpE=";
-  #     };
-  #   }))
 ]
